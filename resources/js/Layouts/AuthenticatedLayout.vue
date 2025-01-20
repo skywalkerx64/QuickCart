c8b9fb8c6d69cdff2fe45901ac993fbcb15d8328
skywalkerx64 <rickjoaodonald@gmail.com>
2025-01-20 20:28:18 +0100

    wip

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -83,7 +83,7 @@ const user = computed(() => page.props.auth.user);
       </nav>
     </aside>
     <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-      <header class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <header class="sticky top-0 z-30 flex h-14  justify-between items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
         <Sheet>
           <SheetTrigger as-child>
             <Button size="icon" variant="outline" class="sm:hidden">
@@ -121,11 +121,6 @@ const user = computed(() => page.props.auth.user);
         </Sheet>
 
         <slot name="header" />
-
-        <div class="relative ml-auto flex items-center md:grow-0">
-          <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input type="search" placeholder="Search..." class="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]" />
-        </div>
         <ToggleTheme />
         <UserProfile :user="user" />
       </header>
